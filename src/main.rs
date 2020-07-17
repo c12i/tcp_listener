651846a446a43ee0f58db2a0631036d643385ec4
Collins Muriuki <murerwacollins@gmail.com>
2020-07-17 16:48:48 +0300

    Initial data model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Server {
+            address: address.to_string()
+        }
+    }
+
+    fn run(self) {
+        println!("Server running on http://{}", self.address);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new(tostr!("127.0.0.1:8080"));
+    server.run();
+}
+
+#[macro_export]
+macro_rules! tostr {
+    ($input:expr) => {
+        String::from($input)
+    };
 }
