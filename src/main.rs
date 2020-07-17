08822d3c71ff0204097cd3945e401461432c059a
Collins Muriuki <murerwacollins@gmail.com>
2020-07-17 18:14:21 +0300

    Modules

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
-struct Server {
-    address: String,
+mod server {
+    pub struct Server {
+        address: String,
+    }
+    
+    impl Server {
+        pub fn new(address: String) -> Self {
+            Server {
+                address: address.to_string()
+            }
+        }
+    
+        pub fn run(self) {
+            println!("Server running on http://{}", self.address);
+        }
+    }
 }
 
-impl Server {
-    fn new(address: String) -> Self {
-        Server {
-            address: address.to_string()
+mod http {
+    mod request {
+        pub struct Request {
+            path: String,
+            query: Option<String>,
+            method: super::method::Method,
         }
     }
 
-    fn run(self) {
-        println!("Server running on http://{}", self.address);
+    mod response {
+
+    }
+
+    mod method {
+        #[derive(Debug)]
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            PATCH,
+            DELETE,
+            OPTIONS,
+            TRACE,
+            CONNECT,
+            HEAD
+        }
     }
 }
 
-fn main() {
-    let server = Server::new(tostr!("127.0.0.1:8080"));
-    server.run();
-}
+use server::Server;
 
-#[macro_export]
-macro_rules! tostr {
-    ($input:expr) => {
-        String::from($input)
-    };
+fn main() {
+    let server = Server::new(String::from("127.0.0.1:8080"));
+    server.run();
 }
