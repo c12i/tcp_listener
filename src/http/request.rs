9e18e0c16d1236d225fffa1fdd98937be34d64dc
Collins Muriuki <murerwacollins@gmail.com>
2020-07-18 20:38:41 +0300

    Pattern match to get query string and path

--- src/http/request.rs
+++ src/http/request.rs
@@ -25,14 +25,27 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(value)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
 
-        let method:Result<Method, _> = method.parse();
+        let method:Method = method.parse()?;
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i];
+        }
+
+        match path.find('?') {
+            Some(i) => {
+                query_string = Some(&path[i+1..]);
+                path = &path[..i];
+            },
+            None => {}
+        }
 
         unimplemented!()
     }
