7cb90685eea1aae95380e6c9586ea6c6840f87d0
Collins Muriuki <murerwacollins@gmail.com>
2020-07-19 20:23:35 +0300

    Clean up pattern matching with if let

--- src/http/request.rs
+++ src/http/request.rs
@@ -23,28 +23,24 @@ impl TryFrom<&[u8]> for Request {
 
     // to => GET /search?name=example&sort=1 HTTP/1.1
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        // convert &[u8] to &str
         let request = str::from_utf8(value)?;
+
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-
+        // check http type
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
 
         let method:Method = method.parse()?;
+
         let mut query_string = None;
+
         if let Some(i) = path.find('?') {
             query_string = Some(&path[i+1..]);
             path = &path[..i];
-        }
-
-        match path.find('?') {
-            Some(i) => {
-                query_string = Some(&path[i+1..]);
-                path = &path[..i];
-            },
-            None => {}
         }
 
         unimplemented!()
