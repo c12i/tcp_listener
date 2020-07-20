d68e8d0f662c30547994cb862ee0217bb2deeb3e
Collins Muriuki <murerwacollins@gmail.com>
2020-07-20 19:51:08 +0300

    Intialize Response struct

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -4,5 +4,6 @@ pub mod method;
 mod query_string;
 
 pub use request::{Request,ParseError};
+pub use response::Response;
 pub use method::Method;
 pub use query_string::{QueryString,Value as QueryStringValue};
---
+++ src/http/response.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+pub struct Response {
+    status: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status: StatusCode, body: Option<String>) -> Self {
+        Response {
+            status,
+            body,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum StatusCode {}
