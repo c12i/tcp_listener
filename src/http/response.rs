7b6c32d72ab293717147f9a41c39c7b4931c0031
Collins Muriuki <murerwacollins@gmail.com>
2020-07-20 23:30:07 +0300

    Implement Display trait to Response

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display,Formatter,Result as FmtResult};
 use super::StatusCode;
 
 #[derive(Debug)]
@@ -12,5 +13,15 @@ impl Response {
             status,
             body,
         }
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        let body = match &self.body {
+            Some(b) => b,
+            None => ""
+        };
+        write!(f, "HTTP/1.1 {} {}\r\n\r\n{}", self.status, self.status.reason_phrase(),body)
     }
 }
