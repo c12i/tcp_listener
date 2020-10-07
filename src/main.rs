0c7028d2ac135eec925e4f0bd1faf013c62e9468
Collins Muriuki <murerwacollins@gmail.com>
2020-10-08 01:04:52 +0300

    Keep crate clean of any dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ mod utils;
 mod request_handler;
 
 use std::env;
-use macroz::tostr;
 use server::Server;
 use request_handler::RequestHandler;
 
@@ -14,6 +13,6 @@ fn main() {
     // read env variables at compile time
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    let server = Server::new(tostr!("127.0.0.1:8080"));
+    let server = Server::new(String::from("127.0.0.1:8080"));
     server.run(RequestHandler::new(public_path));
 }
